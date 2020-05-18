8c7a5625ac76e494914f97c6eb5a29b9480d98f7
Jack Ditto <jack@Jacks-MacBook-Pro.local>
2020-05-18 14:56:02 -0500

    Clean up, comment

---
+++ Game.py
@@ -0,0 +1,86 @@
+class Game:
+
+    def __init__(self):
+        # Total score for a game
+        self.total_score = 0
+        self.board = [
+            # [0, 0],  Top Score hole - in practice doesn't matter
+            [0, 0],  # 0
+            [0, 0],  # 1
+            [0, 0],  # 2
+            [0, 0],  # 3
+            [0, 0],  # 4
+            [0, 0],  # 5
+        ]
+
+        # Move is ongoing
+        self.moving = False
+
+    def move(self, hole):
+        '''
+        Called to initiate a move. Initiate a move by choosing a hole on your side.
+        '''
+
+        # Num beads in selected hole
+        num_beads = self.board[hole][0]
+
+        # Set hole to 0 since you are "picking up" the beads
+        self.board[hole][0] = 0
+
+        # Set starting index to next hole
+        curr_index, side = self.next_index(hole, 0)
+
+        # Recursively move until we land on a score or on a 0 hole
+        # self.move_recursive(curr_index, num_beads, side)
+
+        # Keep the beads moving
+        while self.moving:
+
+            # Placing beads in hand
+            for i in range(num_beads):
+
+                # TODO: Handle scoring
+
+                # Add bead to board
+                self.board[curr_index][side] += 1
+
+                # TODO: check if last bead is on your score zone or on a hole with beads.
+                # Set self.moving accordingly
+
+                # Get the next hole
+                curr_index, side = self.next_index(curr_index, side)
+
+    def next_index(self, curr_index, curr_side):
+
+        if curr_side == 0:
+
+            if curr_index == 5:
+                return curr_index, 1
+
+            return curr_index + 1, 0
+
+        if curr_side == 1:
+
+            if curr_index == 0:
+                return curr_index, 0
+
+            return curr_index - 1, 1
+
+    def printGame(self):
+        for i in range(len(self.board)):
+            print(self.board[i])
+
+        print(f"  {self.total_score}")
+
+    def setBoard(self, board=[]):
+
+        if board:
+            self.board = board
+            return
+
+        for s in range(2):
+            print("Home side...")
+            for i in range(len(self.board)-1):
+
+                p = int(input(f"Number is pos {i+1}"))
+                self.board[i][s] = p
